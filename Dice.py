83fc62652e63a6a7e453e12c5c4a5c8d34a0246b
Olivia <oliviayzhou@gmail.com>
2022-12-27 00:44:57 -0500

    item classes

--- Dice.py
+++ Dice.py
@@ -5,6 +5,8 @@ def dice(size, times=None):
     else:
         return [random.randint(1,size) for _ in range(times)]
 
+def d100(times=None):
+    return dice(100, times)
 def d20(times=None):
     return dice(20, times)
 def d10(times=None):
@@ -15,3 +17,9 @@ def d6(times=None):
     return dice(6, times)
 def d4(times=None):
     return dice(4, times)
+
+def mutable(parameter, default_val):
+    if parameter is None:
+        return default_val
+    else:
+        return parameter
--- Items.py
+++ Items.py
@@ -1,21 +1,40 @@
 class Item:
     """
-
+    Things to put in the bag. Not necessarily useful.
     """
     def __init__(self, name, description, value):
+        """
+
+        :param name: Item name
+        :param description: item description
+        :param value: Market value of item. Sale price will vary based on charisma.
+        """
         self.name = name
         self.description = description
         self.value = value
 
-class Food(Item):
-    def __init__(self, name, description, value):
-        super().__init__(name, description, value)
-
 class Consumable(Item):
-    def __init__(self, name, description, value):
+    def __init__(self, name, description, value, effect):
+        """
+        :param effect: list tuple [(effected stat, effect size, over turns)] eg [[hp, 2, 5], [mp, -5, 1]]
+        """
         super().__init__(name, description, value)
+        self.effect = effect
+
+class Food(Consumable):
+    def __init__(self, name, description, value, effect, hunger):
+        """
+        :param hunger: amount of hunger recovered
+        """
+        super().__init__(name, description, value, effect)
+        self.hunger = hunger
 
 class Equipment(Item):
-    def __init__(self, description, value, modifier, equipment_type):
-        super().__init__(description, value, modifier)
+    def __init__(self, name, description, value, equipment_type, modifier):
+        """
+        :param equipment_type: Type of equipment. from [helmet, armor, shoes, weapon, top, bot, accessories]
+        :param modifier: stats modified. {def: 20, magic_def: 30, hp:20, mp:-5, charisma: 99}
+        """
+        super().__init__(name, description, value)
         self.equipment_type = equipment_type
+        self.modifier = modifier
